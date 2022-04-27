f986a01d22004c1bb809858bc4810da623b4648d
Martin <martingiacosa@gmail.com>
2022-04-27 14:45:24 -0300

    Act

--- classes/Email.php
+++ classes/Email.php
@@ -19,62 +19,58 @@ class Email {
 
     public function enviarConfirmacion() {
 
-         // create a new object
-         $mail = new PHPMailer();
-         $mail->isSMTP();
-         $mail->Host = 'smtp.mailtrap.io';
-         $mail->SMTPAuth = true;
-         $mail->Port = 2525;
-         $mail->Username = '25281089791d5d';
-         $mail->Password = 'e967dc1bfdaeb9';
-     
-         $mail->setFrom('cuentas@appsalon.com');
-         $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
-         $mail->Subject = 'Confirma tu Cuenta';
+        $mail = new PHPMailer();
+        $mail->SMTPSecure = 'tls';
+        $mail->Username = $_ENV['MAIL_USER'];
+        $mail->Password = $_ENV['MAIL_PASS'];
+        $mail->AddAddress($this->email,'AppSalon.com');
+        $mail->FromName = "AppSalon";
+        $mail->Subject = "Confirmacion de Alta de Cuenta";
+        $mail->Host = "smtp.live.com";
+        $mail->Port = 587;
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true;
+        $mail->From = $mail->Username;
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Creado tu cuenta en 
+        AppSalon, solo debes confirmarla en el siguiente enlace </P>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>";
+        $contenido .= '</html>';
+        $mail->Body = $contenido;
+        $mail->send();
 
-         // Set HTML
-         $mail->isHTML(TRUE);
-         $mail->CharSet = 'UTF-8';
-
-         $contenido = '<html>';
-         $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta en App Salón, solo debes confirmarla presionando el siguiente enlace</p>";
-         $contenido .= "<p>Presiona aquí: <a href='https://rocky-woodland-49408.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
-         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
-         $contenido .= '</html>';
-         $mail->Body = $contenido;
-
-         //Enviar el mail
-         $mail->send();
 
     }
 
     public function enviarInstrucciones() {
 
-        // create a new object
         $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPSecure = 'tls';
+        $mail->Username = $_ENV['MAIL_USER'];
+        $mail->Password = $_ENV['MAIL_PASS'];
+        $mail->AddAddress($this->email,'AppSalon.com');
+        $mail->FromName = "AppSalon";
+        $mail->Subject = 'Reestablece tu Password';
+        $mail->Host = "smtp.live.com";
+        $mail->Port = 587;
+        $mail->IsSMTP();
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = '25281089791d5d';
-        $mail->Password = 'e967dc1bfdaeb9';
-    
-        $mail->setFrom('cuentas@appsalon.com');
-        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
-        $mail->Subject = 'Reestablece tu password';
+        $mail->From = $mail->Username;
 
-        // Set HTML
         $mail->isHTML(TRUE);
         $mail->CharSet = 'UTF-8';
-
         $contenido = '<html>';
-        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='https://rocky-woodland-49408.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
-        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Parece que has olvidado 
+        tu Password, sigue el siguiente enlace para recuperarla </P>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/recuperar?token=" . 
+        $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
+        $mail->send(); 
 
-            //Enviar el mail
-        $mail->send();
     }
 }
